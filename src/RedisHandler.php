66aac1f02707a03f5c40fa4a6d7dfed8cceec968
yangbiao <yangbiao@huanya.com>
2023-05-12 15:40:55 +0700

    1

---
+++ src/RedisHandler.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @desc RedisHanle.php 描述信息
+ * @author Tinywan(ShaoBo Wan)
+ * @date 2022/3/18 17:13
+ */
+
+declare(strict_types=1);
+
+namespace Tinywan\Jwt;
+
+use support\Redis;
+use Tinywan\Jwt\Exception\JwtCacheTokenException;
+
+class RedisHandler
+{
+    /**
+     * @desc: 生成设备缓存令牌
+     * （1）登录时，判断该账号是否在其它设备登录，如果有，就请空之前key清除，
+     * （2）重新设置key 。然后存储用户信息和ip地址拼接为key，存储在redis当中
+     * @param array $args
+     * @author Tinywan(ShaoBo Wan)
+     */
+    public static function generateToken(array $args): void
+    {
+        $cacheKey = $args['cache_token_pre'] . $args['id'];
+        $key = Redis::keys($cacheKey . ':*');
+        if (!empty($key)) {
+            Redis::del(current($key));
+        }
+        Redis::setex($cacheKey, $args['cache_token_ttl'], $args['access_token']);
+    }
+
+    /**
+     * @desc: 检查设备缓存令牌
+     * @param string $pre
+     * @param string $uid
+     * @param string $token
+     * @return bool
+     * @author Tinywan(ShaoBo Wan)
+     */
+    public static function verifyToken(string $pre, string $uid, string $token): bool
+    {
+        $cacheKey = $pre . $uid;
+        if (!Redis::exists($cacheKey)) {
+            throw new JwtCacheTokenException('该账号已在其他设备登录，强制下线');
+        }
+        if (Redis::get($cacheKey) != $token) {
+            throw new JwtCacheTokenException('身份验证令牌已失效');
+        }
+        return true;
+    }
+
+    /**
+     * @desc: 清理缓存令牌
+     * @param string $pre
+     * @param string $uid
+     * @return bool
+     * @author Tinywan(ShaoBo Wan)
+     */
+    public static function clearToken(string $pre, string $uid): bool
+    {
+        $token = Redis::keys($pre . $uid . ':*');
+        if ($token) {
+            Redis::del(current($token));
+        }
+        return true;
+    }
+}
